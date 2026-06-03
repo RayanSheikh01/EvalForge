bcc7165467250c1d90dc52e615a7ed68c3fbe060
RayanSheikh01 <w2094027@westminster.ac.uk>
2026-06-03 11:51:19 +0500

    add judge scorer and hallucination scorer into registry

--- src/evalforge/scoring/registry.py
+++ src/evalforge/scoring/registry.py
@@ -2,14 +2,18 @@
 
 
 from evalforge.scoring.completion import CompletionScorer
+from evalforge.scoring.judge import JudgeScorer
 from evalforge.scoring.latency import LatencyScorer
 from evalforge.scoring.token_cost import TokenCostScorer
 from evalforge.scoring.tool_accuracy import ToolAccuracyScorer
+from evalforge.scoring.hallucination import HallucinationScorer
 
 
 DEFAULT_SCORERS = {
     "latency": LatencyScorer(),
     "token_cost": TokenCostScorer(),
     "completion": CompletionScorer(),
-    "tool_accuracy": ToolAccuracyScorer()
+    "tool_accuracy": ToolAccuracyScorer(),
+    "judge": JudgeScorer(),
+    "hallucination": HallucinationScorer(),
 }
